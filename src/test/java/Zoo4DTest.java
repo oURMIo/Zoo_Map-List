34ae67d7ac5493c396e5633ebb2127097a07ae81
oURMIo <dimach.98.ru@gmail.com>
2022-06-29 16:28:38 +0300

    I did it 2

--- src/test/java/Zoo4DTest.java
+++ src/test/java/Zoo4DTest.java
@@ -1,43 +1,28 @@
-import animal.Animal;
 import animal.AnimalType;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
-import java.util.Objects;
 import java.util.logging.Logger;
 
 import static animal.AnimalType.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Zoo4DTest {
     private final Logger logger = Logger.getLogger(Zoo4DTest.class.getName());
 
-    /**
-     * for compare two Animal obj
-     *
-     * @param need - Animal obj
-     * @param now - Animal obj
-     * @return 0 or -1
-     */
-    int compareAnimal(Animal need, Animal now) {
-        if (Objects.equals(need, now)) {
-            return 0;
-        }
-        return -1;
-    }
-
     @ParameterizedTest
     @EnumSource(AnimalType.class)
-    void findBag() {
+    void findBug() {
         var zoo = new StrategicalZoo();
         var animal = WOLF.create("Radion");    /*   for assertEquals   */
         zoo.addAnimal(WOLF.create("Radion"));  /* in list 0 */
         zoo.addAnimal(BEAR.create("Kosolap"));  /* in list 1 */
         zoo.addAnimal(LION.create("Lion"));  /* in list 2 */
-        assertEquals(0, compareAnimal(animal, zoo.getAnimalById(0)));
-//        logger.info("/////   OLD    ///// -    " + zoo.getAnimalById(0));
+        assertEquals(animal, zoo.getAnimalById(0));
+        logger.info("/////   OLD    ///// -    " + zoo.getAnimalById(0));
         zoo.removeAnimal(zoo.getAnimalById(0));
-//        logger.info("/////   NOW    ///// -    " + zoo.getAnimalById(0));
-        assertEquals(0, compareAnimal(null, zoo.getAnimalById(0)));
+        logger.info("/////   NOW    ///// -    " + zoo.getAnimalById(0));
+        assertNull(zoo.getAnimalById(0));
+
     }
 }
