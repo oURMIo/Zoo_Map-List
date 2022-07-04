b9eff0c798f990f94c5669759075478dd63d6138
Anton Chistyakov <Anton.Chistyakov@fxdd.com>
2022-07-04 14:16:19 +0300

    Add test on decorator integration

---
+++ src/test/java/ZooDecoratorDTest.java
@@ -0,0 +1,33 @@
+import animal.Animal;
+
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+import static animal.AnimalType.ELEPHANT;
+import static animal.AnimalType.LION;
+import static animal.AnimalType.WOLF;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class ZooDecoratorDTest {
+    private final Logger logger = Logger.getLogger(ZooDecoratorDTest.class.getName());
+
+    @Test
+    void findBug() {
+        var zoo = new NamedZoo(new StrategicalZoo(a -> a.getType() != ELEPHANT, b -> b.getType() != LION));
+
+        zoo.addAnimal(WOLF.create("Gray"));
+        var pink = ELEPHANT.create("Pink");
+        zoo.addAnimal(pink);
+        var king = LION.create("King");
+        zoo.addAnimal(king);
+
+        assertEquals(2, zoo.getAnimalCount(), "all but elephant is added");
+        assertNull(zoo.geAnimalsByName(pink.getName()), "Pink elephant is expected to be out of zoo");
+
+        zoo.removeAnimal(king);
+        assertEquals(2, zoo.getAnimalCount(), "lion is not removed");
+        assertNotNull(zoo.geAnimalsByName("King"), "King is expected to be in zoo");
+    }
+}
